d12aba559e10e07b07f06d3f47d7b23a9972102d
Usman <usmanyahaya6967@gmail.com>
2023-12-26 15:01:52 +0100

    add border on sidebar

--- includes/student_sidebar.php
+++ includes/student_sidebar.php
@@ -1,65 +1,65 @@
  <!-- Sidebar Menu -->
  <nav class="mt-2">
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <li class="nav-item">
+          <li class="nav-item border-bottom">
             <a href="dashboard" class="nav-link">
-              <i class="nav-icon bi bi-speedometer mr-1 text-warning"></i>
+              <i class="nav-icon bi bi-speedometer mr-3 text-warning"></i>
               <p>
                 Dashboard
               </p>
             </a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item border-bottom">
             <a href="student_profile" class="nav-link">
-              <i class="bi bi-person mr-1 text-warning"></i>
+              <i class="bi bi-person mr-3 text-warning"></i>
               <p>
-                MyProfile
+                Profile
               </p>
             </a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item border-bottom">
             <a href="olevel" class="nav-link">
-              <i class="bi bi-mortarboard mr-1 text-warning"></i>
+              <i class="bi bi-mortarboard mr-3 text-warning"></i>
               <p>
               O'Level
               </p>
             </a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item border-bottom">
             <a class="nav-link" href="jamb_result">
-              <i class="bi bi-book mr-1 text-warning"></i>
+              <i class="bi bi-book mr-3 text-warning"></i>
               <p>
               JAMB
               </p>
             </a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item border-bottom">
             <a href="program" class="nav-link">
-              <i class="bi bi-list-check mr-1 text-warning"></i>
+              <i class="bi bi-list-check mr-3 text-warning"></i>
               <p>
               Program
               </p>
             </a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item border-bottom">
             <a href="payment" class="nav-link">
-              <i class="bi bi-credit-card mr-1 text-warning"></i>
+              <i class="bi bi-credit-card mr-3 text-warning"></i>
               <p>
               Payment
               </p>
             </a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item border-bottom">
             <a href="acknowledgment_slip" class="nav-link">
-              <i class="bi bi-file-earmark-check mr-1 text-warning"></i>
+              <i class="bi bi-file-earmark-check mr-3 text-warning"></i>
               <p>
               Acknowledgement Slip
               </p>
             </a>
           </li>
-          <li class="nav-item">
+          <li class="nav-item border-bottom">
             <a href="logout" class="nav-link">
-              <i class="bi bi-person-circle mr-1 text-warning"></i>
+              <i class="bi bi-person-circle mr-3 text-warning"></i>
               <p>
                 Logout
               </p>
